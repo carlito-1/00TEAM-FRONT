aeaff3ae5b6590a8389b26814c13812666d4c1f7
Carlos <carlos.carra7@gmail.com>
2025-06-15 00:48:55 +0200

    front

---
+++ front/jugadores.js
@@ -0,0 +1,117 @@
+const datosPorTemporada = {
+    "2024/2025": [
+    {
+  nombre: "Juan",
+  posicion: "Defens",
+  equipo: "00 TEAM",
+  dorsal: 5,
+  partidos: 19,
+  goles: 7,
+  asistencias: 7,
+  mvp : 2,
+  foto: "public/juan.png"
+},
+{
+  nombre: "Carralero",
+  posicion: "Defensa",
+  equipo: "00 TEAM",
+  dorsal: 3,
+  partidos: 22,
+  goles: 1,
+  asistencias: 3,
+  mvp : 1,
+  foto: "public/carralero.png"
+},
+{
+  nombre: "Nico",
+  posicion: "Delantero",
+  equipo: "00 TEAM",
+  dorsal: 8,
+  partidos: 15,
+  goles: 14,
+  asistencias: 9,
+  mvp : 6,
+  foto: "public/nico.png"
+},
+{
+  nombre: "Ivan",
+  posicion: "Portero",
+  equipo: "00 TEAM",
+  dorsal: 21,
+  partidos: 16,
+  goles: 0,
+  asistencias: 0,
+  mvp : 2,
+  foto: "public/ivan.png"
+},
+{
+  nombre: "Alberto",
+  posicion: "Banda",
+  equipo: "00 TEAM",
+  dorsal: 9,
+  partidos: 15,
+  goles: 10,
+  asistencias: 7,
+  mvp : 1,
+  foto: "public/alberto.png"
+},
+{
+  nombre: "Pedro",
+  posicion: "Mediocentro",
+  equipo: "00 TEAM",
+  dorsal: 10,
+  partidos: 22,
+  goles: 6,
+  asistencias: 3,
+  mvp : 1,
+  foto: "public/pedro.png"
+},
+{
+  nombre: "Eneoo",
+  posicion: "Banda",
+  equipo: "00 TEAM",
+  dorsal: 11,
+  partidos: 13,
+  goles: 7,
+  asistencias: 5,
+  mvp : 3,
+  foto: "public/eneoo.png"
+},
+{
+  nombre: "Dani",
+  posicion: "Mediocentro",
+  equipo: "00 TEAM",
+  dorsal: 7,
+  partidos: 16,
+  goles: 9,
+  asistencias: 4,
+  mvp : 1,
+  foto: "public/dani.png"
+},
+{
+  nombre: "Victor",
+  posicion: "Defensa",
+  equipo: "00 TEAM",
+  dorsal: 7,
+  partidos: 14,
+  goles: 3,
+  asistencias: 2,
+  mvp : 0,
+  foto: "public/victor.png"
+},
+{
+  nombre: "Jora",
+  posicion: "Delantero",
+  equipo: "00 TEAM",
+  dorsal: 20,
+  partidos: 21,
+  goles: 7,
+  asistencias: 2,
+  mvp : 1,
+  foto: "public/jora.png"
+}
+    ],
+  "2025/2026" : [
+    
+  ]
+};
