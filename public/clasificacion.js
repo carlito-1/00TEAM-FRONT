a5973dff15e1b9af470fa48d3e361be01d737863
Carlos <carlos.carra7@gmail.com>
2025-06-16 01:38:11 +0200

    public

---
+++ public/clasificacion.js
@@ -0,0 +1,30 @@
+const calificacionesPorTemporada = {
+    "2024/2025": [
+      { nombre: "PICHAREAL", puntos: 56, pj: 21, pg: 18, pe: 2, pp: 1, gf: 96, gc: 28 },
+      { nombre: "FULHAM BEER", puntos: 49, pj: 21, pg: 17, pe: 0, pp: 3, gf: 9, gc: 5 },
+      { nombre: "MACAKINHOS", puntos: 46, pj: 21, pg: 16, pe: 0, pp: 4, gf: 6, gc: 7 },
+      { nombre: "00 TEAM", puntos: 42, pj: 21, pg: 13, pe: 3, pp: 5, gf: 10, gc: 4 },
+      { nombre: "PERICOS", puntos: 37, pj: 21, pg: 12, pe: 1, pp: 8, gf: 54, gc: 54 },
+      { nombre: "SPORTING BIELAS", puntos: 30, pj: 21, pg: 10, pe: 2, pp: 8, gf: 57, gc: 63 },
+      { nombre: "AERONABOS", puntos: 21, pj: 21, pg: 6, pe: 3, pp: 12, gf: 37, gc: 74 },
+      { nombre: "A4", puntos: 17, pj: 21, pg: 4, pe: 5, pp: 12, gf: 38, gc: 52 },
+      { nombre: "LAT RELAMIDAS", puntos: 15, pj: 21, pg: 4, pe: 3, pp: 14, gf: 30, gc: 64 },
+      { nombre: "VODKA JUNIORS", puntos: 8, pj: 21, pg: 1, pe: 5, pp: 15, gf: 30, gc: 77 },
+      { nombre: "LA RESISTENCIA", puntos: 6, pj: 21, pg: 2, pe: 2, pp: 16, gf: 30, gc: 96 },
+      { nombre: "LOS INCUMPLIDOS", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0 }
+    ],
+    "2025/2026": [
+      { nombre: "00 TEAM", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0 },
+      { nombre: "DEFAULT", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0 },
+      { nombre: "DEFAULT", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0 },
+      { nombre: "DEFAULT", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0 },
+      { nombre: "DEFAULT", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0 },
+      { nombre: "DEFAULT", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0 },
+      { nombre: "DEFAULT", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0 },
+      { nombre: "DEFAULT", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0 },
+      { nombre: "DEFAULT", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0 },
+      { nombre: "DEFAULT", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0 },
+      { nombre: "DEFAULT", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0 },
+      { nombre: "DEFAULT", puntos: 0, pj: 0, pg: 0, pe: 0, pp: 0, gf: 0, gc: 0 },
+    ]
+  };
---
+++ public/temporadas.js
@@ -0,0 +1,261 @@
+const equipo = {
+    nombre: "00 TEAM",
+    temporadas: [
+      {
+        año: "2024/2025",
+        jornadas: [
+          {
+            numero: 1,
+            local: "00 TEAM",
+            rival: "VODKA JRS",
+            resultado: "6-1",
+            jugadores: [
+              { nombre: "Nico", goles: 2 },
+              { nombre: "Pedro", goles: 2 },
+              { nombre : "Alex", goles : 1},
+              { nombre : "Dani", goles : 1}
+            ]
+          },
+          {
+            numero: 2,
+            local: "00 TEAM",
+            rival: "A4",
+            resultado: "1-1",
+            jugadores: [
+              { nombre: "Alberto", goles: 1 },
+            ]
+          },
+          {
+              numero : 3,
+              local : "00 TEAM",
+              rival : "SPORTING BIELAS",
+              resultado : "3-2",
+              jugadores : [
+                  { nombre : "Nico", goles : 2},
+                  { nombre : "Pedro", goles : 1}
+              ]
+          },
+          {
+              numero : 4,
+              local : "00 TEAM",
+              rival : "FULHAM BEER",
+              resultado : "2-4",
+              jugadores : [
+                  { nombre : "Alberto", goles : 1},
+                  { nombre : "Arevalo", goles : 1}
+              ]
+          },
+          {
+              numero : 5,
+              local : "00 TEAM",
+              rival : "LAT RELAMIDAS",
+              resultado : "5-0",
+              jugadores : [
+                  { nombre : "Nico", goles : 3},
+                  { nombre : "Pedro", goles : 1},
+                  { nombre : "Jora", goles : 1}
+              ]
+          },
+          {
+              numero : 6,
+              local : "00 TEAM",
+              rival : "LA RESISTENCIA",
+              resultado : "3-0",
+              jugadores : [
+                  { nombre : "Nico", goles : 2},
+                  { nombre : "Eneoo", goles : 1}
+              ]
+          },
+          {
+              numero : 7,
+              local : "00 TEAM",
+              rival : "LOS INCUMPLIDOS",
+              resultado : "3-3",
+              jugadores : [
+                  { nombre : "Nico", goles : 1},
+                  { nombre : "Dani", goles : 1},
+                  { nombre : "Juan", goles : 1}
+              ]
+          },
+          {
+              numero : 8,
+              local : "00 TEAM",
+              rival : "LOS PERICOS",
+              resultado : "3-2",
+              jugadores : [
+                  { nombre : "Juan", goles : 2},
+                  { nombre : "Oscar", goles : 1}
+              ]
+          },
+          {
+              numero : 9,
+              local : "00 TEAM",
+              rival : "MAKACINHOS",
+              resultado : "2-1",
+              jugadores : [
+                  { nombre : "Nico", goles : 1},
+                  { nombre : "Juan", goles : 1}
+              ]
+          },
+          {
+              numero : 10,
+              local : "00 TEAM",
+              rival : "PICHAREAL",
+              resultado : "4-7",
+              jugadores : [
+                  { nombre : "Nico", goles : 1},
+                  { nombre : "Mosquera", goles : 1},
+                  { nombre : "Dani", goles : 1},
+                  { nombre : "Propia Puerta", goles : 1}
+  
+              ]
+          },
+          {
+              numero : 11,
+              local : "00 TEAM",
+              rival : "AERONABOS",
+              resultado : "3-2",
+              jugadores : [
+                  { nombre : "Nico", goles : 1},
+                  { nombre : "Dani", goles : 1},
+                  { nombre : "Pedro", goles : 1}
+              ]
+          },
+          {
+              numero : 12,
+              local : "00 TEAM",
+              rival : "VODKA JRS",
+              resultado : "2-0",
+              jugadores : [
+                  { nombre : "Dani", goles : 1},
+                  { nombre : "Eneoo", goles : 1}
+              ]
+          },
+          {
+              numero : 13,
+              local : "00 TEAM",
+              rival : "A4",
+              resultado : "1-0",
+              jugadores : [
+                  { nombre : "Dani", goles : 1}
+                  
+              ]
+          },
+          {
+              numero : 14,
+              local : "00 TEAM",
+              rival : "SPORTING BIELAS",
+              resultado : "3-5",
+              jugadores : [
+                  { nombre : "Nico", goles : 1},
+                  { nombre : "Dani", goles : 1},
+                  { nombre : "Juan", goles : 1}            
+              ]
+          },
+          {
+              numero : 15,
+              local : "00 TEAM",
+              rival : "FULHAM BEER",
+              resultado : "1-7",
+              jugadores : [
+                  { nombre : "Alberto", goles : 2}
+                  
+              ]
+          },
+          {
+              numero : 16,
+              local : "00 TEAM",
+              rival : "LAT RELAMIDAS",
+              resultado : "8-2",
+              jugadores : [
+                  { nombre : "Dani", goles : 2},
+                  { nombre : "Albrto", goles : 1},
+                  { nombre : "Pedro", goles : 1},
+                  { nombre : "Jora", goles : 1},
+                  { nombre : "Eneoo", goles : 1},
+                  { nombre : "Victor", goles : 1},
+                  { nombre : "Alex", goles : 1}
+  
+              ]
+          },
+          {
+              numero : 17,
+              local : "00 TEAM",
+              rival : "LA REISTENCIA",
+              resultado : "7-1",
+              jugadores : [
+                  { nombre : "Jora", goles : 4},
+                  { nombre : "Juan", goles : 1},
+                  { nombre : "Arevalo", goles : 1},
+                  { nombre : "Vazquez", goles : 1}
+              ]
+          },
+          {
+              numero : 18,
+              local : "00 TEAM",
+              rival : "LOS INCUMPLIDOS",
+              resultado : "5-1",
+              jugadores : [
+                  { nombre : "Eneoo", goles : 2},
+                  { nombre : "Alberto", goles : 1},
+                  { nombre : "Dani", goles : 1},
+                  { nombre : "Victor", goles : 1}
+              ]
+          },
+          {
+              numero : 19,
+              local : "00 TEAM",
+              rival : "LOS PERICOS",
+              resultado : "6-2",
+              jugadores : [
+                  { nombre : "Carralero", goles : 1},
+                  { nombre : "Propia Puerta", goles : 1},
+                  { nombre : "Eneoo", goles : 2},
+                  { nombre : "Vazquez", goles : 1},
+                  { nombre : "Alberto", goles : 1}
+              ]
+          },
+          {
+              numero : 20,
+              local : "00 TEAM",
+              rival : "MACAKINHOS",
+              resultado : "0-1",
+              jugadores : [
+              
+              ]
+          },
+          {
+              numero : 21,
+              local : "00 TEAM",
+              rival : "PICHAREAL",
+              resultado : "2-2",
+              jugadores : [
+                  { nombre : "Alberto", goles : 2}
+                  
+              ]
+          },
+          {
+              numero : 22,
+              local : "00 TEAM",
+              rival : "AERONABOS",
+              resultado : "9-1",
+              jugadores : [
+                  { nombre : "Jora", goles : 1},
+                  { nombre : "Alberto", goles : 2},
+                  { nombre : "Vazquez", goles : 4},
+                  { nombre : "Juan", goles : 1},
+                  { nombre : "Arevalo", goles : 1}
+              ]
+          },
+        
+        ]
+      },
+      {
+        año: "2025/2026",
+        jornadas: [
+          
+         
+        ]
+      },
+    ]
+  };
